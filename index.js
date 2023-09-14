55cd2ef2b4c1697cf78d8874817b1d4a1f7dc153
Ruslana17 <ruslana.fankovskaya@gmail.com>
2023-09-14 14:16:11 +0200

    update

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const playPause = document.getElementById('play_pause');
 const nextSong = document.getElementById('next_song');
 const previousSong = document.getElementById('previous_song');
 
+const audio =document.getElementById('song');
+const imagePlayuer= document.getElementById('image_album');
+const imageBackground = document.getElementById('image_album');
+
 const songs =['songs/beyonce.mp3', 'songs/dontstartnow.mp3'];
 const imageAlbum = ['img/lemonade.png','img/dontstartnow.png'];
 
-console.log(imageAlbum);
-console.log(songs);
+
 
 
 /*let isPlaying = fasle;
