371816c22d4757e7f6206e3c7ac1bcdca6cf55da
Ruslana17 <ruslana.fankovskaya@gmail.com>
2023-09-14 12:47:27 +0200

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+const playPause = document.getElementById('play_pause');
+const nextSong = document.getElementById('next_song');
+const previousSong =document.getElementById('previous_song');
+
+/*console.log(playPause, nextSong, previousSong);*/
