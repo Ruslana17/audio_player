51b63ebac208a5042ef7930653c8031106750b8f
Ruslana17 <ruslana.fankovskaya@gmail.com>
2023-09-14 14:45:37 +0200

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const previousSong = document.getElementById('previous_song');
 
 const audio =document.getElementById('song');
 const imagePlayuer= document.getElementById('image_album');
-const imageBackground = document.getElementById('image_album');
+const imageBackground = document.getElementById('backround-image');
 
 const songs =['songs/beyonce.mp3', 'songs/dontstartnow.mp3'];
 const imageAlbum = ['img/lemonade.png','img/dontstartnow.png'];
@@ -13,8 +13,11 @@ let currentSongIndex=0;
 
 function loadSong(index){
     audio.src=songs[index];
+    imagePlayuer.src=imageAlbum[index];
+    imageBackground.src=imageAlbum[index];
 }
 loadSong(currentSongIndex);
+
 
 function playPauseFunstion(){
     if(audio.paused){
@@ -29,3 +32,10 @@ function playPauseFunstion(){
 playPause.addEventListener('click', playPauseFunstion);
 
 
+nextSong.addEventListener('click', () =>{
+    currentSongIndex=(currentSongIndex + 1) % songs.length;
+    loadSong(currentSongIndex);
+    if(!audio.paused){
+        audio.play();
+    }
+})
